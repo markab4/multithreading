a9401a4a121791e1e80fddf37cc6a9cf20f7528a
Mark Abramov <markabramov01@gmail.com>
2019-07-11 14:24:22 -0400

    single threaded solution

---
+++ ImageProcessing.java
@@ -0,0 +1,101 @@
+// recolors an image (of flowers in this case) from white to purple
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageProcessing {
+    public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+    public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
+
+
+    public static void main(String[] args) throws IOException {
+        File source = new File(SOURCE_FILE);
+        BufferedImage originalImage = ImageIO.read(source);
+        BufferedImage resultImage = new BufferedImage(
+                originalImage.getWidth(),
+                originalImage.getHeight(),
+                BufferedImage.TYPE_INT_RGB);
+
+        recolorSingleThreaded(originalImage, resultImage);
+
+        File outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+    }
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage){
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner,
+                                    int width, int height){
+        for(int x = leftCorner; x < leftCorner + width && x < originalImage.getWidth(); x++) {
+            for(int y = topCorner; y < topCorner + height && y<originalImage.getHeight(); y++){
+                recolorPixel(originalImage, resultImage, x, y);
+            }
+        }
+    }
+
+    public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y){
+
+        int rgb = originalImage.getRGB(x, y),
+                red = getRed(rgb),
+                blue = getBlue(rgb),
+                green = getGreen(rgb),
+                newRed,
+                newGreen,
+                newBlue;
+
+        // purple is a combination of red and blue, so we decrease amount of green
+        if(isShadeOfGray(red, green, blue)) {       // our target values are shades of grey
+            newRed = Math.min(255, red + 10);
+            newGreen = Math.max(0, green - 80);
+            newBlue = Math.max(0, blue - 20);
+        } else {
+            newRed = red;
+            newGreen = green;
+            newBlue = blue;
+        }
+        int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+        setRGB(resultImage, x, y, newRGB);
+    }
+
+    public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+        image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+    }
+
+
+    //checking that all three components have a similar color intensity,
+    // ie if no one particular component is stronger than the rest.
+    public static boolean isShadeOfGray(int red, int green, int blue){
+        // if the color is almost a perfect mix of green red and blue, it's a shade of gray.
+        return  Math.abs(red - green) < 30 &&
+                Math.abs(red - blue) < 30 &&
+                Math.abs(green - blue) < 30;
+    }
+
+    public static int createRGBFromColors(int red, int green, int blue){
+        int rgb = 0;
+        rgb |= blue;
+        rgb |= green << 8;
+        rgb |= red << 16;
+
+        rgb |= 0xFF000000;
+        return rgb;
+    }
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getBlue(int rgb) {
+        return rgb & 0x000000FF;
+    }
+
+}
