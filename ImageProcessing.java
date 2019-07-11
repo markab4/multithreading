f87376902c865cae2c6e32800e8fac51f495c165
Mark Abramov <markabramov01@gmail.com>
2019-07-11 15:01:28 -0400

    multi threaded solution, plus runtime of each threaded solution

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -4,6 +4,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageProcessing {
     public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
@@ -18,14 +20,63 @@ public class ImageProcessing {
                 originalImage.getHeight(),
                 BufferedImage.TYPE_INT_RGB);
 
-        recolorSingleThreaded(originalImage, resultImage);
+
+        // HOW LONG THREADED PROCESS TAKES
+
+        // single-threaded process = 1080 ms
+        // multi-threaded process with 1 thread = 1157 ms
+        // multi-threaded process with 2 threads = 1001 ms
+        // multi-threaded process with 6 threads = 624 ms
+        // multi-threaded process with 16 threads = 773 ms
+        // multi-threaded process with 40 threads = 1160 ms
+
+
+        long startTime = System.currentTimeMillis();
+//        recolorSingleThreaded(originalImage, resultImage);
+
+        int numberOfThreads = 1;
+        recolorMultithreaded(originalImage, resultImage, numberOfThreads);
+
+        long duration = System.currentTimeMillis() - startTime;
 
         File outputFile = new File(DESTINATION_FILE);
         ImageIO.write(resultImage, "jpg", outputFile);
+
+        System.out.println(duration);
     }
 
     public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage){
         recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorMultithreaded(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads){
+        List<Thread> threads = new ArrayList<>();
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight() / numberOfThreads;
+
+        for(int i = 0; i < numberOfThreads; i++) {
+            final int threadMultiplier = i;
+
+            Thread thread = new Thread(() -> {
+                int leftCorner = 0;
+                int topCorner = height * threadMultiplier;
+
+                recolorImage(originalImage, resultImage, leftCorner, topCorner, width, height);
+            });
+
+            threads.add(thread);
+        }
+
+        for(Thread thread : threads) {
+            thread.start();
+        }
+        for(Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner,
